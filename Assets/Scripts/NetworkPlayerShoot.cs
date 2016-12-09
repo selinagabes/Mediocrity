f722690f900cd29fdce21ee8e4e73b4064b1135b
Saman <arifs@uwindsor.ca>
2016-12-09 16:40:42 -0500

    #1 Raycasting Works

---
+++ Assets/Scripts/NetworkPlayerShoot.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class NetworkPlayerShoot : NetworkBehaviour {
+
+	public NetworkPlayerWeapon RayGun;
+	private bool dirRight = true;
+
+	[SerializeField]
+	private LayerMask mask;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		if (isLocalPlayer) 
+		{
+			if (Input.GetKeyDown ("space")) {
+				if (CrossPlatformInputManager.GetAxis ("Horizontal") > 0)
+					dirRight = true;
+				else if (CrossPlatformInputManager.GetAxis ("Horizontal") < 0)
+					dirRight = false;
+			
+				FireTheLaser ();
+			}
+		}
+	}
+
+	void FireTheLaser()
+	{
+		Ray ray;
+		RaycastHit _hit;
+
+		if (dirRight) 
+		{	
+			ray = new Ray (transform.position, Vector3.right);
+		} 
+		else 
+		{
+			ray = new Ray (transform.position, Vector3.left);
+		}
+
+		if(Physics.Raycast(ray, out _hit, RayGun.range, mask))
+		{
+			Debug.Log ("We hit " + _hit.collider.name);
+		}
+				
+	}
+}
---
+++ Assets/Scripts/NetworkPlayerWeapon.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class NetworkPlayerWeapon {
+
+	public string name = "Raygun";
+	public float damage = 25f;
+	public float range = 50f;
+}
