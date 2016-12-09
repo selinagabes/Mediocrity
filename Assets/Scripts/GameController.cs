e31638869b5731665c8f974cc0315c28391cb93d
selinagabes <gabrieleselina@gmail.com>
2016-12-08 23:36:19 -0500

    #8 teethstairs on fleek

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,8 +16,8 @@ public class GameController : MonoBehaviour {
         MeshGenerator meshGen = GameObject.FindGameObjectWithTag("LevelPath").GetComponent<MeshGenerator>();
         minVertex = meshGen.GetMinimumVertex();
         path = meshGen.GetWalls().Where(w=>w.z <= minVertex.z+5 && w.x > minVertex.x).OrderBy(w=>w.x).ToList();
-        > ToothSpawns = new Dictionary<Vector3, GameObject;
-        > StairSpawns = new Dictionary<Vector3, GameObject;
+        ToothSpawns = new Dictionary<Vector3, GameObject>();
+        StairSpawns = new Dictionary<Vector3, GameObject>();
         PlaceTeeth();
         PlaceStairs();
         ReadjustMap();  
@@ -32,12 +32,13 @@ public class GameController : MonoBehaviour {
         int max = numOfVertexes / 20;
         int index = Random.Range(min, max);
 
-        
-        
+
+       
         for (int i =1; i< 19; i++)
         {
             Vector3 spawnPoint = new Vector3(path[index].x, path[index].z + 1, meshGen.wallHeight / 2);
-            Instantiate(Tooth, spawnPoint, new Quaternion());
+            GameObject currentTooth= (GameObject)Instantiate(Tooth, spawnPoint, new Quaternion());
+            ToothSpawns.Add(spawnPoint, currentTooth);
             min = max;
             max += (numOfVertexes / 20);
             index = Random.Range(min, max);
@@ -48,21 +49,43 @@ public class GameController : MonoBehaviour {
         MeshGenerator meshGen = GameObject.FindGameObjectWithTag("LevelPath").GetComponent<MeshGenerator>();
 
         int numOfVertexes = path.Count;
-        int min = (int)minVertex.x +10;
+        int min = (int)minVertex.x + 10;
         int max = numOfVertexes / 3;
         int index = Random.Range(min, max);
 
         for (int i = 1; i <= 3; i++)
         {
-            Vector3 spawnPoint = new Vector3(path[index].x, path[index].z, 2.5);
-            Instantiate(Stairs, spawnPoint, new Quaternion());
-            min = max;
-            max += (numOfVertexes / 3);
+            Vector3 spawnPoint = new Vector3(path[index].x, path[index].z+2f, 2.5f);
+            GameObject currentStairs =(GameObject) Instantiate(Stairs, spawnPoint, new Quaternion());
+            StairSpawns.Add(spawnPoint, currentStairs);
+            min = max+10;
+            max += (numOfVertexes-10 / 3);
             index = Random.Range(min, max);
         }
     }
     void ReadjustMap()
     {
+        ReplaceTeeth();
+        ResetMeshBelowStairs();
+    }
 
+    void ReplaceTeeth()
+    {
+        foreach (var clone in StairSpawns)
+        {
+            if (ToothSpawns.Any(t => t.Key.x >= clone.Key.x - 6 && t.Key.x <= clone.Key.x + 6))
+            {
+
+                foreach (var tClone in ToothSpawns)
+                {
+                    if (tClone.Key.x >= clone.Key.x - 6 && tClone.Key.x <= clone.Key.x + 6)
+                    {
+                        Destroy(ToothSpawns[tClone.Key].gameObject);
+                    }
+                }
+                Instantiate(TeethStairs, clone.Key, new Quaternion());
+                Destroy(StairSpawns[clone.Key].gameObject);
+            }
+        }
     }
 }
