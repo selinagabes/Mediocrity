c9038169fd563649b2ac924fcfbca5df73921e0d
Saman <arifs@uwindsor.ca>
2016-12-09 15:55:43 -0500

    #1 Adding network game manager

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -3,16 +3,53 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class NetworkPlayer : NetworkBehaviour {
-	
-	public int playerNumber;
+
+	[SerializeField]
+	string remoteLayerName = "RemotePlayer";
+
+	[SerializeField]
+	string localLayerName = "LocalPlayer";
 
 	void Start () 
 	{
-		if (isLocalPlayer)
+		if (isLocalPlayer) 
+		{
 			GetComponent<NetworkPlayerMove> ().enabled = true;
-
-		if (isLocalPlayer) //change to player prefs
 			this.GetComponentInChildren<TextMesh> ().text = "Player";
+		}
+
+		SetupCamera ();
+
+		//Set up Layers
+		if (isLocalPlayer) 
+		{
+			AssignLocalPlayer ();
+		} 
+		else 
+		{
+			AssignRemotePlayer ();
+		}
+	}
+
+	void SetupCamera()
+	{
+		if (isLocalPlayer)
+		{
+			Camera.main.transform.position = this.transform.position - this.transform.forward * 25 + this.transform.up * 3;
+			Camera.main.transform.LookAt(this.transform.position);
+			Camera.main.transform.parent = this.transform;
+		}
+
+	}
+
+	void AssignLocalPlayer()
+	{
+		gameObject.layer = LayerMask.NameToLayer (localLayerName);
+	}
+
+	void AssignRemotePlayer()
+	{
+		gameObject.layer = LayerMask.NameToLayer (remoteLayerName);
 	}
 
 	void Update()
